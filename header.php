29777a6e632bdc372d7dcc6df7ec7fac603237ca
Timur-Basirov <153905042+Timur-Basirov@users.noreply.github.com>
2026-03-31 20:27:43 +0300

    Lisatud alusstruktuur

---
+++ header.php
@@ -0,0 +1,3 @@
+<header>
+    <h1>Timur Baširov PHP tööd | TARpv24</h1>
+</header>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP tööd Timur Baširov(TARpv24)</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+<?php include ('header.php'); ?>
+<div class="flex-container">
+    <?php include ('nav.php'); ?>
+    <div>
+        <?php
+        if(isset($_GET["link"])){
+            include ('content/'.$_GET["link"]);
+        } else {
+            include ('content/Avaleht.php');
+        }
+        ?>
+    </div>
+    <div>
+        <img src="image/pilt.png" alt="pilt vabal valikul">
+        <h1>Kuupäev ja kellaeg(JavaScript)</h1>
+        <button onclick="naita()">Praegu on:</button>
+        <p id="vastus1"></p>
+        <p id="vastus2"></p>
+    </div>
+</div>
+<?php include ('footer.php'); ?>
+<script src="js/musicJs.js"></script>
+<script src="js/nupp.js"></script>
+</body>
+</html>
