f5a86e0d91905124b3a0cd0f48de9e8b427a7f4c
Timur-Basirov <153905042+Timur-Basirov@users.noreply.github.com>
2026-03-31 20:28:18 +0300

    Lisati navigeerimismenüü

---
+++ nav.php
@@ -0,0 +1,22 @@
+<nav>
+    <ul>
+        <li><a href="?link=Avaleht.php">Avaleht</a></li>
+        <li><a href="?link=gitKasutamine.php">Git käsud</a></li>
+        <li><a href="?link=musicVorm.php">JS: Muusika küsitlus</a></li>
+        <li><a href="?link=Objektid.php">Objektid</a></li>
+        <li><a href="?link=ajafunktsioonid.php">Ajafunktsioonid</a></li>
+
+        <li class="has-dropdown">
+            <a href="#">PHP funktsioonid</a>
+            <ul>
+                <li><a href="?link=tekstfunktsioonid.php">Tekstfunktsioonid</a></li>
+                <li><a href="?link=matemaatika.php">Matemaatika</a></li>
+                <li><a href="?link=galerii.php">Galerii</a></li>
+            </ul>
+        </li>
+
+        <li>
+            <a href="https://timurbasirov23.thkit.ee" target="_blank">Vana index</a>
+        </li>
+    </ul>
+</nav>
